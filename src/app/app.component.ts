80fbf8d3b4fbc0b4600653deab32fc9cdf82fb9c
rig0x0 <make73260@gmail.com>
2024-12-07 20:11:03 -0600

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SaludoComponent } from "./saludo/saludo.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, SaludoComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'mi-proyecto-hola-mundo';
+}
